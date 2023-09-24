0e1bbe32fa0bed011d4aa3d5973ddd4cb7254191
be-bus <encinas.1@hotmail.com>
2023-09-24 00:34:11 -0300

    mejore la funcionalidad de elejir la operacion por terminal

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
 const {lectura,escritura}=require('./modulos/readWrite');
+const suma=require('./modulos/suma');
 const resta=require('./modulos/resta');
-const division= require('./modulos/division')
+const multiplicacion=require('./modulos/multiplicacion');
+const division=require('./modulos/division');
+const eleccion = require('./modulos/eleccion');
 
-const historial=lectura(); // leo el contenido del jason y lo parseo
+const historial=lectura(); //leo el contenido del jason y lo parseo
+let elegido=process.argv[2];
 
 const registro={
-    operacion: 'division',
-    resultado: division(+process.argv[2],+process.argv[3])
+    operacion: elegido,
+    resultado: eleccion(elegido,+process.argv[3],+process.argv[4]),
+    operando1:+process.argv[3], //aguegue que se guarden los elementos que se operan
+    operando2:+process.argv[4]
 };
 
-console.log(division(+process.argv[2],+process.argv[3]));
 historial.push(registro) // cargo el registro al array 
-console.log(historial);// mustra el registro
+console.log(registro);// mustra el registro
 escritura(historial) // escribo en el json
 
